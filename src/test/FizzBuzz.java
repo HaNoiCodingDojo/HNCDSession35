9cfbcec028593fe3470faa986537eb08c27c1b33
hainp <hainp2604@gmail.com>
2012-08-25 16:12:08 +0700

    viet

--- src/test/FizzBuzz.java
+++ src/test/FizzBuzz.java
@@ -5,9 +5,8 @@ public class FizzBuzz {
 	
 	public static int askForFizz(int number)
 	{
-		if ( number == 3) return (1);
 		if (number == 6) return (2);
-		if ( number == 4 ) return (1);		
+		if ( number >= 3 ) return (1);		
 		return(0);
 	}
 
